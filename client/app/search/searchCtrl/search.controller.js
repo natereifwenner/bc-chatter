877c2987d4e7404470239ed285221c14c5571c20
Nate Reif-Wenner <nate.reif-wenner@thomsonreuters.com>
2015-11-19 16:13:44 -0600

    Pushing logic from controller to service

--- client/app/search/searchCtrl/search.controller.js
+++ client/app/search/searchCtrl/search.controller.js
@@ -4,20 +4,9 @@ angular.module('brightcoveRequesterApp')
   .controller('SearchCtrl', function (searchService) {
     var search, vm;
     vm = this;
-    vm.search = {
-      'total' : '',
-      'searchTerm' : '',
-      'items' : [],
-      'totalPage' : 0,
-      'page' : 0,
-      'playlist' : { id: null }
+    vm.results = {
+      'done' : false
     };
-    vm.state = {
-      'done' : false,
-      'many' : false,
-      'currentRange' : '',
-      'pagination' : false,
-    }
     vm.searchOptApi = {};
     vm.noResults = false;
   	vm.request = {
@@ -27,69 +16,18 @@ angular.module('brightcoveRequesterApp')
   	};
   	
     vm.searchKey = function(api,q){
-      //reset state
-      vm.noResults = false;
-      vm.state = {
-        'done' : false,
-        'many' : false,
-        'currentRange' : '',
-        'pagination' : false,
-      }
-
     	searchService.searchAll(api,q).then(function(resp){
-    		if(resp.page_size <= resp.total_count){
-    			var range = resp.items.length;
-    			vm.state.many = true;
-    			vm.state.currentRange = 'Viewing ' + range + ' out of ' +resp.total_count;
-    		}else if(resp.total_count === 0){
-          vm.noResults = true;
-        }
-
-    		vm.search.searchTerm = q;
-    		vm.search.page = resp.page_number;
-    		vm.search.total = resp.total_count;
-    		vm.search.items = resp.items;
-        vm.search.totalPage = resp.total_count / 20;
-
-        vm.state.done = true;
-
-        
-    	});
+        vm.results = resp;
+        vm.results.done = true;
+      });
     }
 
     vm.searchPage = function(q, page){
-    	//reset state
-      vm.noResults = false;
-      vm.state = {
-        'done' : false,
-        'many' : false,
-        'currentRange' : '',
-        'pagination' : false,
-      }
+        vm.results.done = false;
     	searchService.searchNextPage(q, page).then(function(resp){
-    		
-    		vm.search.searchTerm = q;
-    		vm.search.page = resp.page_number;
-    		vm.search.total = resp.total_count;
-    		vm.search.items = resp.items;
-    		vm.search.totalPage = createArray(resp.total_count / 20);
-    		vm.search.page = resp.page_number
-
-        vm.state.done = true;
-        vm.state.pagination = true;
-
+        vm.results = resp;
+        vm.results.done = true;
     	});
-    }
-
-    
-
-    //TODO: figure out way to limit pagination
-    var createArray = function(pages){
-    	var x = [];
-		for (var i=1;i<=pages + 1;i++) {
-		  x.push(i);
-		}
-		return x;
     }
 
   });
--- client/app/search/searchService/searchService.service.js
+++ client/app/search/searchService/searchService.service.js
@@ -14,7 +14,16 @@ angular.module('brightcoveRequesterApp')
       else if(api === 'Video'){
   			return $http.get('/api/search/video?q=' + q)
   						.then(function(result){
-              console.log(JSON.stringify(result.data));
+                if(result.data.page_size <= result.data.total_count){
+                  var range = result.data.items.length;
+                  result.data.many = true;
+                  result.data.currentRange = 'Viewing ' + range + ' out of ' +result.data.total_count;
+                }else if(result.data.total_count === 0){
+                  result.data.noResults = true;
+                }
+
+                result.data.searchTerm = q;
+                result.data.totalPage = result.data.total_count / 20;
   							return result.data;
   						})
       }
@@ -22,15 +31,31 @@ angular.module('brightcoveRequesterApp')
     service.searchNextPage = function(q, page){
       return $http.get('/api/search/video?q=' + q + '&page=' + page)
             .then(function(result){
+                result.data.searchTerm = q;
+                result.data.page = result.data.page_number;
+                result.data.total = result.data.total_count;
+                result.data.items = result.data.items;
+                result.data.totalPage = createArray(result.data.total_count / 20);
+                result.data.page = result.data.page_number
+                result.data.pagination = true;
                 return result.data;
             })
     },
     service.getPlaylist = function(id){
         return $http.get('/api/search/playlist?id=' + id)
             .then(function(result){
-                console.log(result.data);
                 return result.data;
             })
+    }
+
+
+    //TODO: figure out way to limit pagination
+    function createArray(pages){
+      var x = [];
+    for (var i=1;i<=pages + 1;i++) {
+      x.push(i);
+    }
+    return x;
     }
   	
     return service;
